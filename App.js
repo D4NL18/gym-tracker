eb040a7b70b601a5d32e4d6c2280e450f99cf3da
Daniel Marinho <danielmarinho8@hotmail.com>
2024-01-18 08:54:45 -0300

    [FEAT][TituloGrande] - Titulo Grande finalizado
    Titulo Grande
    Added font family
    Added Linear Gradiant

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import TituloGrande from './src/components/TituloGrande/TituloGrande';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    'KeaniaOne-Regular': require('./assets/fonts/KeaniaOne-Regular.ttf'),
+  });
+  if (!fontsLoaded) {
+    return undefined
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <LinearGradient
+      colors={[
+        'rgba(235,0,255,0.85)',   // EB00FF em 85%
+        'rgba(173,9,238,0.85)',   // AD09EE em 85%
+        'rgba(173,9,238,1)',      // AD09EE em 100%
+        'rgba(173,9,238,0.85)',   // AD09EE em 85%
+        'rgba(235,0,255,0.8)'     // EB00FF em 80%
+      ]}
+      locations={[0, 0.25, 0.5, 0.75, 1]}
+      style={styles.container}
+    >
+      <TituloGrande texto="Login" />
       <StatusBar style="auto" />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },

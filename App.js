1ac1578cfc311a591bfc11210a57167d982556fb
Daniel Marinho <danielmarinho8@hotmail.com>
2024-03-06 12:00:06 -0300

    [FEAT][routes] - Rotas

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StyleSheet, TouchableOpacity } from 'react-native';
 import { useFonts } from 'expo-font';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Texto from './src/components/Texto/Texto';
 import InputAndLabel from './src/components/InputAndLabel/InputAndLabel';
@@ -13,11 +15,11 @@ import Table from './src/components/Table/Table';
 import ButtonGoBack from './src/components/ButtonGoBack/ButtonGoBack';
 import Modal from './src/components/Modal/Modal';
 
+import Menu from './src/views/Menu/Menu';
 import Login from './src/views/Login/Login';
 import Cadastro from './src/views/Cadastro/Cadastro';
 import SendEmail from './src/views/RecuperarSenha/SendEmail';
 import NewPassword from './src/views/RecuperarSenha/NewPassword';
-import Menu from './src/views/Menu/Menu';
 import TreinarAgora from './src/views/TreinarAgora/TreinarAgora';
 import TreinoDeHoje from './src/views/TreinarAgora/TreinoDeHoje';
 import Treinos from './src/views/Treinos_e_Exercicios/Treinos';
@@ -28,111 +30,73 @@ import AdicionarTreino from './src/views/AdicionarTreino/AdicionarTreino';
 
 export default function App() {
 
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const openModal = () => setModalVisible(true);
-  const closeModal = () => setModalVisible(false);
+  const Stack = createNativeStackNavigator()
 
   let [fontsLoaded] = useFonts({
+
     'KeaniaOne-Regular': require('./assets/fonts/KeaniaOne-Regular.ttf'),
+
   });
+
   if (!fontsLoaded) {
     return undefined
   }
 
-
-
-  items = [
-    { label: 'Footbal', value: 'footballl' },
-    { label: 'Baseball', value: 'baseball' },
-    { label: 'Hockey', value: 'hockey' },
-  ]
-
-  const data = [
-    { exercicio: 'Item 1', carga: '20', tipo: 'carga' },
-    { exercicio: 'Item 2', carga: '30', tipo: 'carga' },
-    { exercicio: 'Item 3', carga: '40', tipo: 'carga' },
-    { exercicio: 'Item 4', carga: '20', tipo: 'carga' },
-    { exercicio: 'Item 5', carga: '30', tipo: 'carga' },
-    { exercicio: 'Item 6', carga: '40', tipo: 'carga' },
-    { exercicio: 'Item 7', carga: '20', tipo: 'carga' },
-    { exercicio: 'Item 8', carga: '30', tipo: 'carga' },
-    { exercicio: 'Item 9', carga: '40', tipo: 'carga' },
-    { exercicio: 'Item 10', carga: '20', tipo: 'carga' },
-    { exercicio: 'Item 11', carga: '30', tipo: 'carga' },
-    { exercicio: 'Item 12', carga: '40', tipo: 'carga' },
-    { exercicio: 'Item 13', carga: '20', tipo: 'carga' },
-    { exercicio: 'Item 14', carga: '30', tipo: 'carga' },
-    { exercicio: 'Item 15', carga: '40', tipo: 'carga' },
-    { exercicio: 'Item 16', carga: '20', tipo: 'carga' },
-    { exercicio: 'Item 17', carga: '30', tipo: 'carga' },
-    { exercicio: 'Item 18', carga: '40', tipo: 'carga' },
-  ];
-
-  const data2 = [
-    { exercicio: 'Item 1', tipo: 'EditAndRemove' },
-    { exercicio: 'Item 2', tipo: 'edit' },
-    { exercicio: 'Item 3', tipo: 'remove' },
-  ];
   return (
-    <LinearGradient
-      colors={[
-        'rgba(235,0,255,0.85)',   // EB00FF em 85%
-        'rgba(173,9,238,0.85)',   // AD09EE em 85%
-        'rgba(173,9,238,1)',      // AD09EE em 100%
-        'rgba(173,9,238,0.85)',   // AD09EE em 85%
-        'rgba(235,0,255,0.8)'     // EB00FF em 80%
-      ]}
-      locations={[0, 0.25, 0.5, 0.75, 1]}
-      style={styles.container}
-    >
-      {/* <Texto texto="Login" tam="Grande" /> */}
-      {/* <InputAndLabel texto="Nome" placeholder="Daniel" items={items} />
-      <InputAndLabel texto="E-mail" placeholder="email@email.com" items={items} tipo="Select" />
-      <InputAndLabel texto="Senha" placeholder="Senha" items={items} tipo="Senha" />
-      <Button texto="Entrar" />
-      <TextButton texto="Não possui uma conta? Cadastre-se agora!" />
-      <TextButton texto="Esqueceu sua senha? Recupere ela aqui!" /> */}
-      {/* <ItemTable exercicio="Supino Retoaaaaaaaaaaaa" carga="20" tipo="carga" />
-      <ItemTable exercicio="Supino Reto" carga="20" tipo="carga" />
-      <ItemTable exercicio="Supino Reto" tipo="edit" />
-      <ItemTable exercicio="Supino Reto" tipo="EditAndRemove" />
-      <ItemTable exercicio="Supino Retoaaaaaaaaaaaaaaaaaaaa" tipo="remove" /> */}
-      {/* <Table data={data} /> */}
-      {/* <Table data={data2} /> */}
-      {/* <TouchableOpacity onPress={openModal}>
-        <Texto texto="Abrir Modal" TextoBotao="Fechar" />
-      </TouchableOpacity>
-
-      <Modal
-        isVisible={modalVisible}
-        onClose={closeModal}
-        texto="Titulo"
-        label="Label"
-        textoBotao="Fechar"
-        tipo="ModalInput"
-      />
-      <ButtonGoBack />
-      <StatusBar style="auto" /> */}
-      {/* <Login /> */}
-      {/* <Cadastro /> */}
-      {/* <SendEmail /> */}
-      {/* <NewPassword /> */}
-      <Menu />
-      {/* <TreinarAgora /> */}
-      {/* <TreinoDeHoje /> */}
-      {/* <Treinos /> */}
-      {/* <Exercicios /> */}
-      {/* <AdicionarTreino /> */}
-      <ButtonGoBack />
-    </LinearGradient>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Menu"
+          component={Menu}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Cadastro"
+          component={Cadastro}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SendEmail"
+          component={SendEmail}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="NewPassword"
+          component={NewPassword}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="TreinarAgora"
+          component={TreinarAgora}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="TreinoDeHoje"
+          component={TreinoDeHoje}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Treinos"
+          component={Treinos}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Exercicios"
+          component={Exercicios}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="AdicionarTreino"
+          component={AdicionarTreino}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: "center"
-  },
-});
--- src/views/Menu/Menu.js
+++ src/views/Menu/Menu.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 import { View, StyleSheet } from "react-native";
 
+import { LinearGradient } from 'expo-linear-gradient';
+
 import Texto from "../../components/Texto/Texto";
 import InputAndLabel from "../../components/InputAndLabel/InputAndLabel";
 import Button from "../../components/Button/Button";
@@ -9,10 +11,21 @@ import TextButton from "../../components/TextButton/TextButton";
 import { height, width } from "../../components/Sizes/Sizes";
 import Modal from "../../components/Modal/Modal";
 
-export default function Menu() {
+export default function Menu(navigation) {
 
     return (
-        <View style={styles.container}>
+        <LinearGradient
+            colors={[
+                'rgba(235,0,255,0.85)',   // EB00FF em 85%
+                'rgba(173,9,238,0.85)',   // AD09EE em 85%
+                'rgba(173,9,238,1)',      // AD09EE em 100%
+                'rgba(173,9,238,0.85)',   // AD09EE em 85%
+                'rgba(235,0,255,0.8)'     // EB00FF em 80%
+            ]}
+            locations={[0, 0.25, 0.5, 0.75, 1]}
+            style={styles.container}
+        >
+
             <Texto texto="Menu" tam="Grande" />
             <View style={styles.containerBotoes}>
                 <Button texto="Treinar Agora!" />
@@ -20,17 +33,20 @@ export default function Menu() {
                 <Button texto="Evolução" />
             </View>
 
-        </View>
+
+        </LinearGradient>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        alignItems: 'center',
+        justifyContent: "center",
         paddingVertical: height * 0.2
     },
     containerBotoes: {
-        height: height*0.6,
+        height: height * 0.6,
         display: "flex",
         justifyContent: "space-evenly"
     }

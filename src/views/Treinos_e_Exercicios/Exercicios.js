b0d2106573808ffa5d31323973c77a7c753bb4e8
Daniel Marinho <danielmarinho8@hotmail.com>
2024-03-07 13:26:27 -0300

    [FIX[routes] - fix css background exercicios

--- src/views/Treinos_e_Exercicios/Exercicios.js
+++ src/views/Treinos_e_Exercicios/Exercicios.js
@@ -43,6 +43,8 @@ export default function Exercicios({navigation}) {
 
 const styles = StyleSheet.create({
     container: {
-
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
     }
 })
